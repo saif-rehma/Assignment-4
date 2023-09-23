8d0b93f297d3cab02f3ec519b67b4b1925f1d63b
saif-rehma <144496587+saif-rehma@users.noreply.github.com>
2023-09-23 16:13:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//  Allama Iqbal said, “Nations are born in the hearts of poets, they prosper and die in the hands of politicians.”
+console.log(`Allama Iqbal said, “Nations are born in the hearts of poets,
+ they prosper and die in the hands of politicians.”`);
